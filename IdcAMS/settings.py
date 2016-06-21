84c6a269a2af502f08f9a0f876c6a9d7d2bdb4ac
mahaijie <mahaijie123@163.com>
2016-06-21 14:32:38 +0800

    修改setting中LANGUAGE_CODE

--- IdcAMS/settings.py
+++ IdcAMS/settings.py
@@ -121,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'zh-CN'
+LANGUAGE_CODE = 'zh-Hans'
 
 TIME_ZONE = 'Asia/Shanghai'
 
