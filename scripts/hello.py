9c500dbad833a736c1d6d0b25a7fa17eeb29ddf3
mahaijie <mahaijie123@163.com>
2016-07-01 15:56:54 +0800

    添加manage.py命令扩展

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
+# Author:马海杰
+# Email:mahaijie123@163.com
 # 执行方法：python manage.py runscript hello
 # 需要安装django-extension，pip install django-extension
 
 from idcroom.models import Idcroom
+
 def run():
     sqldata = Idcroom.objects.all()
     for data in sqldata:
